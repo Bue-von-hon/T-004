cafd4d4a434e2d90c50e1e3d6cd5712bc7f22f74
Bue-von-hon <dkssudvn2@gmail.com>
2023-02-05 21:11:03 +0900

    try-with-resource, enum 적용

--- part2/src/main/java/client/Client.java
+++ part2/src/main/java/client/Client.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public final class Client {
     public static void main(String[] args) {
-        try {
-            final Socket socket = new Socket();
+        try(final Socket socket = new Socket()) {
             socket.connect(new InetSocketAddress("localhost", 1234));
             final OutputStream outputStream = socket.getOutputStream();
             final String s = "hi";
-            final byte[] data =  s.getBytes("UTF-8");
+            final byte[] data =  s.getBytes(StandardCharsets.UTF_8);
             outputStream.write(data);
         } catch (IOException e) {
             System.out.println(e.toString());
